22c3d704bf4a218b453482d01b568851377bb68a
BirdKeonavong <101718505+Bird9249@users.noreply.github.com>
2024-04-02 15:10:46 +0700

    update: AD

--- src/pages/banner-hero/popups/Popup.tsx
+++ src/pages/banner-hero/popups/Popup.tsx
@@ -221,12 +221,12 @@ export default () => {
                 end: end_time,
               })}
               fallback={
-                <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
+                <span class="text-nowrap bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
                   ບໍ່ໄດ້ໃຊ້ງານ
                 </span>
               }
             >
-              <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+              <span class="text-nowrap bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
                 ດຳເນີນການຢູ່
               </span>
             </Show>
